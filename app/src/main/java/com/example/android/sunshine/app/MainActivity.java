d583c5d7a5456bc7dfda00ba76207fe944fd1cd5
Aaron Young <aarony422@gmail.com>
2016-09-19 16:45:26 -0500

    created array Adapter

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -23,6 +23,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -88,6 +89,11 @@ public class MainActivity extends ActionBarActivity {
                     "Sun 6/29 - Sunny - 20/7"
             };
             List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textview, weekForecast);
+
+
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
